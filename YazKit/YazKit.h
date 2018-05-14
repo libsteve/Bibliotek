c2e101970d5d32ff9be6a47178f9dffcc5d1f0e7
Steven Brunwasser <steve@brun.ws>
2018-05-14 10:11:45 -0700

    Initial Commit

---
+++ YazKit/YazKit.h
@@ -0,0 +1,19 @@
+//
+//  YazKit.h
+//  YazKit
+//
+//  Created by Steve Brunwasser on 5/14/18.
+//  Copyright © 2018 Steve Brunwasser. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for YazKit.
+FOUNDATION_EXPORT double YazKitVersionNumber;
+
+//! Project version string for YazKit.
+FOUNDATION_EXPORT const unsigned char YazKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YazKit/PublicHeader.h>
+
+
