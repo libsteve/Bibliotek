3566ee9a0585aa23c95396d36a8997bd90c0abe1
Steven Brunwasser <steve@brun.ws>
2018-05-25 08:53:04 -0700

    Adding support for accessing MARC fields.

---
+++ Bibliotek/BibRecordField.m
@@ -0,0 +1,13 @@
+//
+//  BibRecordField.m
+//  Bibliotek
+//
+//  Created by Steve Brunwasser on 5/25/18.
+//  Copyright © 2018 Steve Brunwasser. All rights reserved.
+//
+
+#import "BibRecordField.h"
+
+@implementation BibRecordField
+
+@end
