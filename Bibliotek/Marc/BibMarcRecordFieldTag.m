ac76ffb6aeb3536d58cadb305ce0c2dad9b3793e
Steven Brunwasser <steve@brun.ws>
2019-01-29 11:26:25 -0800

    MarcRecordFieldTag can now error from invalid value

--- Bibliotek/Marc/BibMarcRecordFieldTag.m
+++ Bibliotek/Marc/BibMarcRecordFieldTag.m
@@ -6,51 +6,70 @@
 //  Copyright © 2019 Steve Brunwasser. All rights reserved.
 //
 
+#import "BibMarcRecordError.h"
 #import "BibMarcRecordFieldTag.h"
 #import "NSCharacterSet+BibASCIICharacterSet.h"
+#import "NSString+BibCharacterSetValidation.h"
 #import <os/log.h>
 
-#define GUARD(CONDITION) if(!(CONDITION))
+#define guard(predicate) if(!((predicate)))
 
 @implementation BibMarcRecordFieldTag {
 @protected
     NSString *_stringValue;
+    BOOL _isControlFieldTag;
 }
+
+@synthesize stringValue = _stringValue;
+@synthesize isControlFieldTag = _isControlFieldTag;
 
 - (instancetype)init {
-    return [self initWithString:@"000"];
+    return [self initWithString:@"000" error:NULL];
 }
 
-- (instancetype)initWithString:(NSString *)stringValue {
-    static os_log_t log;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        log = os_log_create("brun.steve.Bibliotek.BibMarcRecord", "FieldTag");
-    });
+- (instancetype)initWithString:(NSString *)stringValue error:(NSError *__autoreleasing *)error {
     if (self = [super init]) {
-        GUARD([stringValue length] == 3) {
-            os_log_debug(log, "Invalid field tag \"%{public}@\"", stringValue);
-            os_log_info(log, "Field tags are 3-digit codes");
+        guard([stringValue length] == 3) {
+            guard(error != nil) { return nil; }
+            NSString *const description = [NSString stringWithFormat:@"Invalid record field tag \"%@\"", stringValue];
+            NSString *const reason = @"Record field tags are always 3-digit codes.";
+            *error = [NSError errorWithDomain:BibMarcRecordErrorDomain
+                                         code:BibMarcRecordErrorInvalidCharacterCount
+                                     userInfo:@{ NSLocalizedDescriptionKey : description,
+                                                 NSLocalizedFailureReasonErrorKey : reason }];
             return nil;
         }
-        NSCharacterSet *const westernNumeralCharacterSet = [NSCharacterSet bib_westernNumeralCharacterSet];
-        NSString *const trimmedString = [stringValue stringByTrimmingCharactersInSet:westernNumeralCharacterSet];
-        GUARD([trimmedString length] == 0) {
-            os_log_debug(log, "Invalid field tag \"%{public}@\"", stringValue);
-            os_log_info(log, "Field tags are 3-digit western-numeral codes");
+        guard([stringValue bib_isRestrictedToCharacterSet:[NSCharacterSet bib_ASCIIAlphanumericCharacterSet]
+                                                  inRange:NSRangeFromString(stringValue)]) {
+            guard(error != nil) { return nil; }
+            NSString *const description = [NSString stringWithFormat:@"Invalid record field tag \"%@\"", stringValue];
+            NSString *const reason = @"Record field tags contain only ASCII alphanumeric characters.";
+            *error = [NSError errorWithDomain:BibMarcRecordErrorDomain
+                                         code:BibMarcRecordErrorInvalidCharacterCount
+                                     userInfo:@{ NSLocalizedDescriptionKey : description,
+                                                 NSLocalizedFailureReasonErrorKey : reason }];
             return nil;
         }
         _stringValue = [stringValue copy];
+        _isControlFieldTag = [[stringValue substringWithRange:NSMakeRange(0, 2)] isEqualToString:@"00"];
     }
     return self;
 }
 
-+ (instancetype)fieldTagWithString:(NSString *)stringValue {
-    return [[self alloc] initWithString:stringValue];
++ (instancetype)fieldTagWithString:(NSString *)stringValue error:(NSError *__autoreleasing *)error {
+    return [[self alloc] initWithString:stringValue error:error];
 }
 
 - (instancetype)initWithCoder:(NSCoder *)aDecoder {
-    return [self initWithString:[aDecoder decodeObject]];
+    NSError *error = nil;
+    self = [self initWithString:[aDecoder decodeObject] error:&error];
+    guard(error == nil) {
+        NSString *const description = [error localizedDescription];
+        NSString *const reason = [error localizedFailureReason];
+        [NSException raise:@"BibMarcRecordFieldInvalidTagException" format:@"%@: %@", description, reason];
+        return nil;
+    }
+    return self;
 }
 
 - (void)encodeWithCoder:(NSCoder *)aCoder {
@@ -59,18 +78,16 @@
 
 + (BOOL)supportsSecureCoding { return YES; }
 
-- (BOOL)isEqualToFieldTag:(BibMarcRecordFieldTag *)other {
-    return [_stringValue isEqualToString:[other stringValue]];
+- (BOOL)isEqualToTag:(BibMarcRecordFieldTag *)tag {
+    return [_stringValue isEqualToString:[tag stringValue]];
 }
 
-- (BOOL)isEqual:(id)other
-{
+- (BOOL)isEqual:(id)other {
     return [super isEqual:other]
-        || ([other isKindOfClass:[BibMarcRecordFieldTag class]] && [self isEqualTo:other]);
+        || ([other isKindOfClass:[BibMarcRecordFieldTag class]] && [self isEqualToTag:other]);
 }
 
-- (NSUInteger)hash
-{
+- (NSUInteger)hash {
     return [_stringValue hash];
 }
 
@@ -78,68 +95,72 @@
     return _stringValue;
 }
 
-- (NSComparisonResult)compare:(BibMarcRecordFieldTag *)fieldTag {
-    return [_stringValue compare:[fieldTag stringValue]];
+- (NSComparisonResult)compare:(BibMarcRecordFieldTag *)tag {
+    return [_stringValue compare:[tag stringValue]];
 }
 
 @end
 
 @implementation BibMarcRecordFieldTag (KnownValidFieldTags)
 
-+ (BibMarcRecordFieldTag *)isbnFieldTag {
-    return [[BibMarcRecordFieldTag alloc] initWithString:@"020"];
++ (BibMarcRecordFieldTag *)controlNumberTag {
+    return [[BibMarcRecordFieldTag alloc] initWithString:@"001" error:NULL];
 }
 
-+ (BibMarcRecordFieldTag *)lccFieldTag {
-    return [[BibMarcRecordFieldTag alloc] initWithString:@"050"];
++ (BibMarcRecordFieldTag *)isbnTag {
+    return [[BibMarcRecordFieldTag alloc] initWithString:@"020" error:NULL];
 }
 
-+ (BibMarcRecordFieldTag *)ddcFieldTag {
-    return [[BibMarcRecordFieldTag alloc] initWithString:@"082"];
++ (BibMarcRecordFieldTag *)lccTag {
+    return [[BibMarcRecordFieldTag alloc] initWithString:@"050" error:NULL];
 }
 
-+ (BibMarcRecordFieldTag *)authorFieldTag {
-    return [[BibMarcRecordFieldTag alloc] initWithString:@"100"];
++ (BibMarcRecordFieldTag *)ddcTag {
+    return [[BibMarcRecordFieldTag alloc] initWithString:@"082" error:NULL];
 }
 
-+ (BibMarcRecordFieldTag *)titleFieldTag {
-    return [[BibMarcRecordFieldTag alloc] initWithString:@"245"];
++ (BibMarcRecordFieldTag *)authorTag {
+    return [[BibMarcRecordFieldTag alloc] initWithString:@"100" error:NULL];
 }
 
-+ (BibMarcRecordFieldTag *)editionFieldTag {
-    return [[BibMarcRecordFieldTag alloc] initWithString:@"250"];
++ (BibMarcRecordFieldTag *)titleTag {
+    return [[BibMarcRecordFieldTag alloc] initWithString:@"245" error:NULL];
 }
 
-+ (BibMarcRecordFieldTag *)publicationFieldTag {
-    return [[BibMarcRecordFieldTag alloc] initWithString:@"264"];
++ (BibMarcRecordFieldTag *)editionTag {
+    return [[BibMarcRecordFieldTag alloc] initWithString:@"250" error:NULL];
 }
 
-+ (BibMarcRecordFieldTag *)physicalDescriptionFieldTag {
-    return [[BibMarcRecordFieldTag alloc] initWithString:@"300"];
++ (BibMarcRecordFieldTag *)publicationTag {
+    return [[BibMarcRecordFieldTag alloc] initWithString:@"264" error:NULL];
 }
 
-+ (BibMarcRecordFieldTag *)noteFieldTag {
-    return [[BibMarcRecordFieldTag alloc] initWithString:@"500"];
++ (BibMarcRecordFieldTag *)physicalDescriptionTag {
+    return [[BibMarcRecordFieldTag alloc] initWithString:@"300" error:NULL];
 }
 
-+ (BibMarcRecordFieldTag *)bibliographyFieldTag {
-    return [[BibMarcRecordFieldTag alloc] initWithString:@"504"];
++ (BibMarcRecordFieldTag *)noteTag {
+    return [[BibMarcRecordFieldTag alloc] initWithString:@"500" error:NULL];
 }
 
-+ (BibMarcRecordFieldTag *)summaryFieldTag {
-    return [[BibMarcRecordFieldTag alloc] initWithString:@"520"];
++ (BibMarcRecordFieldTag *)bibliographyTag {
+    return [[BibMarcRecordFieldTag alloc] initWithString:@"504" error:NULL];
 }
 
-+ (BibMarcRecordFieldTag *)subjectFieldTag {
-    return [[BibMarcRecordFieldTag alloc] initWithString:@"650"];
++ (BibMarcRecordFieldTag *)summaryTag {
+    return [[BibMarcRecordFieldTag alloc] initWithString:@"520" error:NULL];
 }
 
-+ (BibMarcRecordFieldTag *)genreFieldTag {
-    return [[BibMarcRecordFieldTag alloc] initWithString:@"655"];
++ (BibMarcRecordFieldTag *)subjectTag {
+    return [[BibMarcRecordFieldTag alloc] initWithString:@"650" error:NULL];
 }
 
-+ (BibMarcRecordFieldTag *)seriesFieldTag {
-    return [[BibMarcRecordFieldTag alloc] initWithString:@"830"];
++ (BibMarcRecordFieldTag *)genreTag {
+    return [[BibMarcRecordFieldTag alloc] initWithString:@"655" error:NULL];
+}
+
++ (BibMarcRecordFieldTag *)seriesTag {
+    return [[BibMarcRecordFieldTag alloc] initWithString:@"830" error:NULL];
 }
 
 @end
