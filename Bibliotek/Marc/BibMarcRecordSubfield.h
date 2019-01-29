d138a0032b3df929b5eb0306bee5c007e0637664
Steven Brunwasser <steve@brun.ws>
2019-01-29 12:36:23 -0800

    Added proper validation to subfield codes

--- Bibliotek/Marc/BibMarcRecordSubfield.h
+++ Bibliotek/Marc/BibMarcRecordSubfield.h
@@ -10,24 +10,73 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/// \brief A portion of data within a record data field.
+/// \discussion Data fields hold data within labeled subfields.
+/// Each subfield's code marks the semantic meaning of its content, which is determined by the record field's tag
+/// as defined in the appropriate MARC 21 format specification.
 NS_SWIFT_NAME(MarcRecord.Subfield)
 @interface BibMarcRecordSubfield : NSObject <NSCopying, NSMutableCopying, NSSecureCoding>
 
+/// A record subfield's code identifies the semantic purpose of the content within a subfield.
+///
+/// The semantics of each code is determined by the record field's tag as defined in the relevant MARC 21 format.
 @property (nonatomic, copy, readonly) NSString *code;
+
+/// A string representation of the information stored in the subfield.
 @property (nonatomic, copy, readonly) NSString *content;
 
-- (instancetype)initWithCode:(NSString *)code content:(NSString *)content NS_DESIGNATED_INITIALIZER;
+/// Create a subfield of data for use within a record's data field.
+/// \param code The subfield code identifying the semantic purpose of the data.
+/// \param content A string representation of the data stored within the subfield.
+/// \returns Returns a subfield value when the given subfield code is well-formatted.
+/// \pre The subfield code must contain exactly one ASCII lowercase or numeric character.
+- (instancetype)initWithCode:(NSString *)code content:(NSString *)content NS_SWIFT_UNAVAILABLE("Use init(code:content:)");
 
+/// Create a subfield of data for use within a record's data field.
+/// \param code The subfield code identifying the semantic purpose of the data.
+/// \param content A string representation of the data stored within the subfield.
+/// \param error An error pointer which can return a reason explaining why the creation of a subfield failed.
+/// \returns Returns a subfield value when the given subfield code is well-formatted.
+/// \pre The subfield code must contain exactly one ASCII lowercase or numeric character.
+- (instancetype)initWithCode:(NSString *)code
+                     content:(NSString *)content
+                       error:(NSError *_Nullable __autoreleasing *_Nullable)error NS_DESIGNATED_INITIALIZER;
+
+/// Create a subfield of data for use within a record's data field.
+/// \param code The subfield code identifying the semantic purpose of the data.
+/// \param content A string representation of the data stored within the subfield.
+/// \param error An error pointer which can return a reason explaining why the creation of a subfield failed.
+/// \returns Returns a subfield value when the given subfield code is well-formatted.
+/// \pre The subfield code must contain exactly one ASCII lowercase or numeric character.
++ (instancetype)subfieldWithCode:(NSString *)code
+                         content:(NSString *)content
+                           error:(NSError *_Nullable __autoreleasing *_Nullable)error NS_SWIFT_UNAVAILABLE("Use init(code:content:)");
+
+/// Create a subfield of data for use within a record's data field.
+/// \param code The subfield code identifying the semantic purpose of the data.
+/// \param content A string representation of the data stored within the subfield.
+/// \returns Returns a subfield value when the given subfield code is well-formatted.
+/// \pre The subfield code must contain exactly one ASCII lowercase or numeric character.
 + (instancetype)subfieldWithCode:(NSString *)code content:(NSString *)content NS_SWIFT_UNAVAILABLE("Use init(code:content:)");
 
-- (BOOL)isEqualToSubfield:(BibMarcRecordSubfield *)other;
+/// Determine if this subfield and its data is equivalent to that of the given subfield.
+/// \param subfield The subfield that is being compaired with this instance for equality.
+/// \returns Returns \c YES when both fields have the same code and content.
+- (BOOL)isEqualToSubfield:(BibMarcRecordSubfield *)subfield;
 
 @end
 
 NS_SWIFT_NAME(MarcRecord.MutableSubfield)
 @interface BibMarcRecordMutableSubfield : BibMarcRecordSubfield
 
+/// A record subfield's code identifies the semantic purpose of the content within a subfield.
+///
+/// The semantics of each code is determined by the record field's tag as defined in the appropriate MARC 21 format.
+///
+/// \note An exception is thrown if the code does not contain exactly one ASCII lowercase or numeric character.
 @property (nonatomic, copy, readwrite) NSString *code;
+
+/// A string representation of the information stored in the subfield.
 @property (nonatomic, copy, readwrite) NSString *content;
 
 @end
--- Bibliotek/Marc/BibMarcRecordSubfield.m
+++ Bibliotek/Marc/BibMarcRecordSubfield.m
@@ -6,15 +6,21 @@
 //  Copyright © 2019 Steve Brunwasser. All rights reserved.
 //
 
+#import "BibMarcRecordError.h"
 #import "BibMarcRecordSubfield.h"
 #import "NSCharacterSet+BibASCIICharacterSet.h"
+#import "NSString+BibCharacterSetValidation.h"
 
-#define BIB_ASSERT_VALID_SUBFILED_CODE(CODE) do {                                                                    \
-    NSAssert([(CODE) length] == 1, @"Invalid subfield code \"%@\": "                                                 \
-             @"Subfield codes must be exactly one lowercase ASCII character", (CODE));                               \
-    NSAssert([[NSCharacterSet bib_lowercaseAlphanumericCharacterSet] characterIsMember:[(CODE) characterAtIndex:0]], \
-             @"Invalid subfield code \"%@\": Subfield codes must be a lowercase ASCII character", (CODE));           \
+#define guard(predicate) if(!((predicate)))
+
+#define BIB_ASSERT_VALID_SUBFILED_CODE(CODE) do {                                                                      \
+    NSAssert([((CODE)) length] == 1, @"Invalid subfield code \"%@\": "                                                 \
+             @"Subfield codes must be exactly one lowercase ASCII character", ((CODE)));                               \
+    NSAssert([[NSCharacterSet bib_lowercaseAlphanumericCharacterSet] characterIsMember:[((CODE)) characterAtIndex:0]], \
+             @"Invalid subfield code \"%@\": Subfield codes must be a lowercase ASCII character", ((CODE)));           \
 } while(0)
+
+static NSString *const BibMarcRecordSubfieldInvalidCodeException = @"BibMarcRecordSubfieldInvalidCodeException";
 
 @implementation BibMarcRecordSubfield {
 @protected
@@ -30,21 +36,40 @@
 }
 
 - (instancetype)initWithCode:(NSString *)code content:(NSString *)content {
+    return [self initWithCode:code content:content error:NULL];
+}
+
+- (instancetype)initWithCode:(NSString *)code content:(NSString *)content error:(NSError *__autoreleasing *)error {
+    guard([BibMarcRecordSubfield validateSubfieldCode:code error:error]) {
+        return nil;
+    }
     if (self = [super init]) {
-        BIB_ASSERT_VALID_SUBFILED_CODE(code);
         _code = [code copy];
         _content = [content copy] ?: @"";
     }
     return self;
 }
 
++ (instancetype)subfieldWithCode:(NSString *)code content:(NSString *)content error:(NSError *__autoreleasing *)error {
+    return [[self alloc] initWithCode:code content:content error:error];
+}
+
 + (instancetype)subfieldWithCode:(NSString *)code content:(NSString *)content {
-    return [[self alloc] initWithCode:code content:content];
+    return [self subfieldWithCode:code content:content error:NULL];
 }
 
 - (instancetype)initWithCoder:(NSCoder *)aDecoder {
-    return [self initWithCode:[aDecoder decodeObjectForKey:@"code"]
-                      content:[aDecoder decodeObjectForKey:@"content"]];
+    NSError *error = nil;
+    self = [self initWithCode:[aDecoder decodeObjectForKey:@"code"]
+                      content:[aDecoder decodeObjectForKey:@"content"]
+                        error:&error];
+    guard(error == nil) {
+        NSString *const description = [error localizedDescription];
+        NSString *const reason = [error localizedFailureReason];
+        [NSException raise:BibMarcRecordSubfieldInvalidCodeException format:@"%@: %@", description, reason];
+        return nil;
+    }
+    return self;
 }
 
 - (id)copyWithZone:(NSZone *)zone {
@@ -79,6 +104,31 @@
     return [_code hash] ^ [_content hash];
 }
 
++ (BOOL)validateSubfieldCode:(NSString *)code error:(NSError *__autoreleasing *)error {
+    guard([code length] == 1) {
+        guard(error != nil) { return NO; }
+        NSString *const description = [NSString stringWithFormat:@"Invalid subfield code \"%@\"", code];
+        NSString *const reason = @"Subfield codes are exactly one character.";
+        *error = [NSError errorWithDomain:BibMarcRecordErrorDomain
+                                     code:BibMarcRecordErrorInvalidCharacterCount
+                                 userInfo:@{ NSLocalizedDescriptionKey : description,
+                                             NSLocalizedFailureReasonErrorKey : reason }];
+        return NO;
+    }
+    guard([code bib_isRestrictedToCharacterSet:[NSCharacterSet bib_ASCIILowercaseAlphanumericCharacterSet]
+                                       inRange:NSRangeFromString(code)]) {
+        guard(error != nil) { return NO; }
+        NSString *const description = [NSString stringWithFormat:@"Invalid subfield code \"%@\"", code];
+        NSString *const reason = @"Subfield codes are ASCII lowercase and numeric characters.";
+        *error = [NSError errorWithDomain:BibMarcRecordErrorDomain
+                                     code:BibMarcRecordErrorInvalidCharacterCount
+                                 userInfo:@{ NSLocalizedDescriptionKey : description,
+                                             NSLocalizedFailureReasonErrorKey : reason }];
+        return NO;
+    }
+    return YES;
+}
+
 @end
 
 @implementation BibMarcRecordMutableSubfield
@@ -89,7 +139,13 @@
     if (_code == code) {
         return;
     }
-    BIB_ASSERT_VALID_SUBFILED_CODE(code);
+    NSError *error = nil;
+    guard([BibMarcRecordSubfield validateSubfieldCode:code error:&error]) {
+        NSString *const description = [error localizedDescription];
+        NSString *const reason = [error localizedFailureReason];
+        [NSException raise:BibMarcRecordSubfieldInvalidCodeException format:@"%@: %@", description, reason];
+        return;
+    }
     [self willChangeValueForKey:@"code"];
     _code = [code copy];
     [self didChangeValueForKey:@"code"];
