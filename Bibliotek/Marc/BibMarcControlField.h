dab1d66b928305082ed943753ab832b00bcf892c
Steven Brunwasser <steve@brun.ws>
2019-02-03 14:13:28 -0800

    Renamed BibMarcRecordControlField to BibMarcControlField

--- Bibliotek/Marc/BibMarcControlField.h
+++ Bibliotek/Marc/BibMarcControlField.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-@class BibMarcRecordFieldTag;
+@class BibMarcTag;
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -16,35 +16,32 @@ NS_ASSUME_NONNULL_BEGIN
 ///
 /// More information about MARC 21 records can be found in the Library of Congress's documentation on
 /// MARC 21 Record Structure: https://www.loc.gov/marc/specifications/specrecstruc.html.
-NS_SWIFT_NAME(MarcRecord.ControlField)
-@interface BibMarcRecordControlField : NSObject <NSCopying, NSMutableCopying, NSSecureCoding>
+NS_SWIFT_NAME(MarcControlField)
+@interface BibMarcControlField : NSObject <NSCopying, NSMutableCopying, NSSecureCoding>
 
 /// A three-digit code used to identify this control field's semantic purpose.
-@property (nonatomic, strong, readonly) BibMarcRecordFieldTag *tag;
+@property (nonatomic, strong, readonly) BibMarcTag *tag;
 
 /// The stored data within this control field.
 @property (nonatomic, copy, readonly) NSString *content;
 
-- (nullable instancetype)initWithTag:(BibMarcRecordFieldTag *)tag
-                             content:(NSString *)content
-                               error:(NSError *__autoreleasing *)error NS_DESIGNATED_INITIALIZER;
+- (nullable instancetype)initWithTag:(BibMarcTag *)tag content:(NSString *)content NS_DESIGNATED_INITIALIZER;
 
-+ (nullable instancetype)controlFieldWithTag:(BibMarcRecordFieldTag *)tag
-                                     content:(NSString *)content
-                                       error:(NSError *__autoreleasing *)error NS_SWIFT_UNAVAILABLE("Use init(tag:content:)");
++ (nullable instancetype)controlFieldWithTag:(BibMarcTag *)tag
+                                     content:(NSString *)content NS_SWIFT_UNAVAILABLE("Use init(tag:content:)");
 
 /// Determine whether or not the given control field represents the same data as the receiver.
 /// \param controlField The control field with which the receiver should be compared.
 /// \returns Returns \c YES if the given control field and the receiver have the same tag and content data.
-- (BOOL)isEqualToControlField:(BibMarcRecordControlField *)controlField;
+- (BOOL)isEqualToControlField:(BibMarcControlField *)controlField;
 
 @end
 
-NS_SWIFT_NAME(MarcRecord.MutableControlField)
-@interface BibMarcRecordMutableControlField : BibMarcRecordControlField
+NS_SWIFT_NAME(MarcMutableControlField)
+@interface BibMarcMutableControlField : BibMarcControlField
 
 /// A three-digit code used to identify this control field's semantic purpose.
-@property (nonatomic, strong, readwrite) BibMarcRecordFieldTag *tag;
+@property (nonatomic, strong, readwrite) BibMarcTag *tag;
 
 /// The stored data within this control field.
 @property (nonatomic, copy, readwrite) NSString *content;
---
+++ Bibliotek/Marc/BibMarcControlField.m
@@ -0,0 +1,120 @@
+//
+//  BibMarcControlField.m
+//  Bibliotek
+//
+//  Created by Steve Brunwasser on 1/22/19.
+//  Copyright © 2019 Steve Brunwasser. All rights reserved.
+//
+
+#import "BibMarcControlField.h"
+#import "BibMarcRecordError.h"
+#import "BibMarcTag.h"
+#import "NSCharacterSet+BibASCIICharacterSet.h"
+#import "NSString+BibCharacterSetValidation.h"
+#import <os/log.h>
+
+#define guard(predicate) if(!((predicate)))
+
+static NSString *const kTagKey = @"tag";
+static NSString *const kContentKey = @"content";
+
+static BOOL sIsValidTag(NSString *tag);
+
+@implementation BibMarcControlField {
+@protected
+    BibMarcTag *_tag;
+    NSString *_content;
+}
+
+@synthesize tag = _tag;
+@synthesize content = _content;
+
+- (instancetype)init {
+    return [self initWithTag:[BibMarcTag controlNumberTag] content:@""];
+}
+
+- (instancetype)initWithTag:(BibMarcTag *)tag content:(NSString *)content {
+    guard([tag isControlFieldTag]) {
+        return nil;
+    }
+    if (self = [super init]) {
+        _tag = [tag copy];
+        _content = [_content copy];
+    }
+    return self;
+}
+
++ (instancetype)controlFieldWithTag:(BibMarcTag *)tag content:(NSString *)content {
+    return [[self alloc] initWithTag:tag content:content];
+}
+
+- (id)copyWithZone:(NSZone *)zone {
+    return [[BibMarcControlField allocWithZone:zone] initWithTag:_tag content:_content];
+}
+
+- (id)mutableCopyWithZone:(NSZone *)zone {
+    return [[BibMarcMutableControlField allocWithZone:zone] initWithTag:_tag content:_content];
+}
+
+#pragma mark - Coding
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    return [self initWithTag:[aDecoder decodeObjectForKey:kTagKey]
+                     content:[aDecoder decodeObjectForKey:kContentKey]];
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+    [aCoder encodeObject:_tag forKey:kTagKey];
+    [aCoder encodeObject:_content forKey:kContentKey];
+}
+
++ (BOOL)supportsSecureCoding { return YES; }
+
+#pragma mark - Equality
+
+- (BOOL)isEqualToControlField:(BibMarcControlField *)other {
+    return [_tag isEqualToTag:[other tag]]
+        && [_content isEqualToString:[other content]];
+}
+
+- (BOOL)isEqual:(id)other {
+    return [super isEqual:other]
+        || ([other isKindOfClass:[BibMarcControlField class]] && [self isEqualToControlField:other]);
+}
+
+- (NSUInteger)hash {
+    return [_tag hash] ^ [_content hash];
+}
+
+@end
+
+#pragma mark -
+
+@implementation BibMarcMutableControlField
+
+@dynamic tag;
++ (BOOL)automaticallyNotifiesObserversOfTag { return NO; }
+- (void)setTag:(BibMarcTag *)tag {
+    if (_tag == tag) {
+        return;
+    }
+    guard ([tag isControlFieldTag]) {
+        return;
+    }
+    [self willChangeValueForKey:kTagKey];
+    _tag = tag;
+    [self didChangeValueForKey:kTagKey];
+}
+
+@dynamic content;
++ (BOOL)automaticallyNotifiesObserversOfContent { return NO; }
+- (void)setContent:(NSString *)content {
+    if (_content == content) {
+        return;
+    }
+    [self willChangeValueForKey:kContentKey];
+    _content = [content copy];
+    [self didChangeValueForKey:kContentKey];
+}
+
+@end
