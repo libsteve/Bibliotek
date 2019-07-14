dc6eabeda876df16127fcc22f9d53184b316dd64
Steven Brunwasser <steve@brun.ws>
2019-07-13 22:00:28 -0700

    Create record

---
+++ Bibliotek/Record/BibRecord.h
@@ -0,0 +1,100 @@
+//
+//  BibRecord.h
+//  Bibliotek
+//
+//  Created by Steve Brunwasser on 7/2/19.
+//  Copyright © 2019 Steve Brunwasser. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BibMetadata.h"
+
+@class BibLeader;
+@class BibControlField;
+@class BibContentField;
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// \brief A collection of information pertaining to some item or entity organized using the MARC 21 standard.
+/// \discussion MARC 21 records are comprised of a leader that contains basic metadata about the record itself, a set of
+/// control fields storing semantic metadata about the record; and a set of data fields that provide the bibliographic
+/// or other data describing the record's item or entity.
+///
+/// More information about MARC 21 records can be found in the Library of Congress's documentation on
+/// MARC 21 Record Structure: https://www.loc.gov/marc/specifications/specrecstruc.html
+NS_SWIFT_NAME(Record)
+@interface BibRecord : NSObject
+
+/// The type of data represented by the record.
+///
+/// MARC 21 records can represent multiple kinds of information—bibliographic, classification, etc.—which each use
+/// different schemas to present their information.
+@property (nonatomic, assign, readonly) BibRecordKind kind;
+
+/// The record's current status in the database it was fetched from.
+@property (nonatomic, assign, readonly) BibRecordStatus status;
+
+@property (nonatomic, copy, readonly) NSArray<BibControlField *> *controlFields;
+
+@property (nonatomic, copy, readonly) NSArray<BibContentField *> *contentFields;
+
+/// Create a MARC 21 record containing data from the given leader, control fields, and data fields.
+///
+/// \returns Returns a valid MARC 21 record for some item or entity described by the given fields.
+/// \discussion More information about MARC 21 records can be found in the Library of Congress's documentation on
+/// MARC 21 Record Structure: https://www.loc.gov/marc/specifications/specrecstruc.html
+- (instancetype)initWithKind:(BibRecordKind)kind
+                      status:(BibRecordStatus)status
+               controlFields:(NSArray<BibControlField *> *)controlFields
+               contentFields:(NSArray<BibContentField *> *)contentFields NS_DESIGNATED_INITIALIZER;
+
+/// Create a MARC 21 record containing data from the given leader, control fields, and data fields.
+///
+/// \returns Returns a valid MARC 21 record for some item or entity described by the given fields.
+/// \discussion More information about MARC 21 records can be found in the Library of Congress's documentation on
+/// MARC 21 Record Structure: https://www.loc.gov/marc/specifications/specrecstruc.html
++ (instancetype)recordWithKind:(BibRecordKind)kind
+                        status:(BibRecordStatus)status
+                 controlFields:(NSArray<BibControlField *> *)controlFields
+                 contentFields:(NSArray<BibContentField *> *)contentFields
+    NS_SWIFT_UNAVAILABLE("Use init(kind:status:controlFields:contentFields:");
+
+@end
+
+#pragma mark - Copying
+
+@interface BibRecord (Copying) <NSCopying, NSMutableCopying>
+@end
+
+#pragma mark - Equality
+
+@interface BibRecord (Equality)
+
+/// Determine whether or not the given MARC 21 record contains the same data as the receiver.
+/// \param record The record with which the receiver should be compared.
+/// \returns Returns \c YES if the given record and the receiver contain the same data
+- (BOOL)isEqualToRecord:(BibRecord *)record;
+
+@end
+
+#pragma mark - Mutable
+
+NS_SWIFT_NAME(MutableRecord)
+@interface BibMutableRecord : BibRecord
+
+/// The type of data represented by the record.
+///
+/// MARC 21 records can represent multiple kinds of information—bibliographic, classification, etc.—which each use
+/// different schemas to present their information.
+@property (nonatomic, assign, readwrite) BibRecordKind kind;
+
+/// The record's current status in the database it was fetched from.
+@property (nonatomic, assign, readwrite) BibRecordStatus status;
+
+@property (nonatomic, copy, readwrite) NSArray<BibControlField *> *controlFields;
+
+@property (nonatomic, copy, readwrite) NSArray<BibContentField *> *contentFields;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Bibliotek/Record/BibRecord.m
@@ -0,0 +1,147 @@
+//
+//  BibRecord.m
+//  Bibliotek
+//
+//  Created by Steve Brunwasser on 7/2/19.
+//  Copyright © 2019 Steve Brunwasser. All rights reserved.
+//
+
+#import "BibRecord.h"
+#import "BibLeader.h"
+#import "BibControlField.h"
+#import "BibContentField.h"
+
+@implementation BibRecord {
+@protected
+    BibRecordKind _kind;
+    BibRecordStatus _status;
+    NSArray<BibControlField *> *_controlFields;
+    NSArray<BibContentField *> *_contentFields;
+}
+
+@synthesize kind = _kind;
+@synthesize status = _status;
+@synthesize controlFields = _controlFields;
+@synthesize contentFields = _contentFields;
+
+- (instancetype)initWithKind:(BibRecordKind)kind
+                      status:(BibRecordStatus)status
+               controlFields:(NSArray<BibControlField *> *)controlFields
+               contentFields:(NSArray<BibContentField *> *)contentFields {
+    if (self = [super init]) {
+        _kind = kind;
+        _status = status;
+        _controlFields = [controlFields copy];
+        _contentFields = [contentFields copy];
+    }
+    return self;
+}
+
+- (instancetype)init {
+    return [self initWithKind:BibRecordKindUndefined
+                       status:BibRecordStatusNew
+                controlFields:[NSArray array]
+                contentFields:[NSArray array]];
+}
+
++ (instancetype)recordWithKind:(BibRecordKind)kind
+                        status:(BibRecordStatus)status
+                 controlFields:(NSArray<BibControlField *> *)controlFields
+                 contentFields:(NSArray<BibContentField *> *)contentFields {
+    return [[self alloc] initWithKind:kind status:status controlFields:controlFields contentFields:contentFields];
+}
+
+- (NSString *)description {
+    NSArray *fields = [[self contentFields] arrayByAddingObjectsFromArray:[self contentFields]];
+    return [fields componentsJoinedByString:@"\n"];
+}
+
+@end
+
+#pragma mark - Copying
+
+@implementation BibRecord (Copying)
+
+- (id)copyWithZone:(NSZone *)zone {
+    return self;
+}
+
+- (id)mutableCopyWithZone:(NSZone *)zone {
+    return [[BibMutableRecord allocWithZone:zone] initWithKind:[self kind]
+                                                        status:[self status]
+                                                 controlFields:[self controlFields]
+                                                 contentFields:[self contentFields]];
+}
+
+@end
+
+#pragma mark - Equality
+
+@implementation BibRecord (Equality)
+
+- (BOOL)isEqualToRecord:(BibRecord *)record {
+    return [self kind] == [record kind]
+        && [self status] == [record status]
+        && [[self controlFields] isEqualToArray:[record controlFields]]
+        && [[self contentFields] isEqualToArray:[record contentFields]];
+}
+
+- (BOOL)isEqual:(id)object {
+    return self == object
+        || ([object isKindOfClass:[BibRecord class]] && [self isEqualToRecord:object]);
+}
+
+- (NSUInteger)hash {
+    return [self kind] ^ ([self status] << 16) ^ [[self controlFields] hash] ^ [[self contentFields] hash];
+}
+
+@end
+
+#pragma mark - Mutable
+
+@implementation BibMutableRecord
+
+- (id)copyWithZone:(NSZone *)zone {
+    return [[BibMutableRecord allocWithZone:zone] initWithKind:[self kind]
+                                                        status:[self status]
+                                                 controlFields:[self controlFields]
+                                                 contentFields:[self contentFields]];
+}
+
+@dynamic kind;
+- (void)setKind:(BibRecordKind)kind {
+    if (_kind != kind) {
+        [self willChangeValueForKey:NSStringFromSelector(@selector(kind))];
+        _kind = kind;
+        [self didChangeValueForKey:NSStringFromSelector(@selector(kind))];
+    }
+}
+
+@dynamic status;
+- (void)setStatus:(BibRecordStatus)status {
+    if (_status != status) {
+        [self willChangeValueForKey:NSStringFromSelector(@selector(status))];
+        _status = status;
+        [self didChangeValueForKey:NSStringFromSelector(@selector(status))];
+    }
+}
+
+@dynamic controlFields;
+- (void)setControlFields:(NSArray<BibControlField *> *)fields {
+    if (_controlFields != fields) {
+        [self willChangeValueForKey:NSStringFromSelector(@selector(controlFields))];
+        _controlFields = [fields copy];
+        [self didChangeValueForKey:NSStringFromSelector(@selector(controlFields))];
+    }
+}
+
+@dynamic contentFields;
+- (void)setContentFields:(NSArray<BibContentField *> *)fields {
+    if (_contentFields != fields) {
+        [self willChangeValueForKey:NSStringFromSelector(@selector(contentFields))];
+        _contentFields = [fields copy];
+        [self didChangeValueForKey:NSStringFromSelector(@selector(contentFields))];
+    }
+}
+
+@end
