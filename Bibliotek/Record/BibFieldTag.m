61cf2babfd85bb85c26d277e9434d8fd281c0f67
Steven Brunwasser <steve@brun.ws>
2019-07-28 11:43:43 -0700

    Make BibFieldTag robust against subclassing

--- Bibliotek/Record/BibFieldTag.m
+++ Bibliotek/Record/BibFieldTag.m
@@ -8,28 +8,25 @@
 
 #import "BibFieldTag.h"
 
+@interface _BibFieldTag : BibFieldTag
+@end
+
 @implementation BibFieldTag
 
 @synthesize stringValue = _stringValue;
 
-static NSCache *sFlyweightCache;
-
-+ (void)initialize {
-    sFlyweightCache = [[NSCache alloc] init];
-    [sFlyweightCache setName:@"BibFieldTagCache"];
++ (instancetype)allocWithZone:(struct _NSZone *)zone {
+    return [self isEqualTo:[BibFieldTag class]]
+         ? [_BibFieldTag allocWithZone:zone]
+         : [super allocWithZone:zone];
 }
 
 - (instancetype)initWithString:(NSString *)stringValue {
     if ([stringValue length] != 3) {
         return nil;
     }
-    BibFieldTag *const tag = [sFlyweightCache objectForKey:stringValue];
-    if (tag) {
-        return tag;
-    }
     if (self = [super init]) {
         _stringValue = [stringValue copy];
-        [sFlyweightCache setObject:self forKey:stringValue];
     }
     return self;
 }
@@ -67,6 +64,33 @@ static NSCache *sFlyweightCache;
 
 - (NSUInteger)hash {
     return [[self stringValue] hash];
+}
+
+@end
+
+#pragma mark -
+
+@implementation _BibFieldTag
+
+static NSCache *sFlyweightCache;
+
++ (void)initialize {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sFlyweightCache = [[NSCache alloc] init];
+        [sFlyweightCache setName:@"_BibFieldTagCache"];
+    });
+}
+
+- (instancetype)initWithString:(NSString *)stringValue {
+    _BibFieldTag *const tag = [sFlyweightCache objectForKey:stringValue];
+    if (tag) {
+        return tag;
+    }
+    if (self = [super initWithString:stringValue]) {
+        [sFlyweightCache setObject:self forKey:stringValue];
+    }
+    return self;
 }
 
 @end
