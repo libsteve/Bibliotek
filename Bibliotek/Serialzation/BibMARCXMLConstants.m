8dbbe87764e256fcc26b5209a7b720c43090f81a
Steven Brunwasser <steve@brun.ws>
2022-05-15 23:44:01 -0700

    Move MARCXML element and attribute constants into a separate file

---
+++ Bibliotek/Serialzation/BibMARCXMLConstants.m
@@ -0,0 +1,24 @@
+//
+//  BibMARCXMLConstants.m
+//  Bibliotek
+//
+//  Created by Steve Brunwasser on 5/8/22.
+//  Copyright © 2022 Steve Brunwasser. All rights reserved.
+//
+
+#import "BibMARCXMLConstants.h"
+
+xmlChar const *const MARCXMLCollection = BAD_CAST "collection";
+xmlChar const *const MARCXMLRecord = BAD_CAST "record";
+xmlChar const *const MARCXMLLeader = BAD_CAST "leader";
+xmlChar const *const MARCXMLControlfield = BAD_CAST "controlfield";
+xmlChar const *const MARCXMLDatafield = BAD_CAST "datafield";
+xmlChar const *const MARCXMLSubfield = BAD_CAST "subfield";
+
+xmlChar const *const MARCXMLTag = BAD_CAST "tag";
+xmlChar const *const MARCXMLInd1 = BAD_CAST "ind1";
+xmlChar const *const MARCXMLInd2 = BAD_CAST "ind2";
+xmlChar const *const MARCXMLCode = BAD_CAST "code";
+
+xmlChar const *const MARCXMLNamespace = BAD_CAST "xmlns";
+xmlChar const *const MARCXMLNamespaceURI = BAD_CAST "http://www.loc.gov/MARC21/slim";
